3a413f4f7f5f51dad9dbc112a84163c7c4d7d43f
mikeyhc <mikeyhc@atmosia.net>
2020-07-19 22:29:22 -0400

    added store meme

--- src/memery_db.erl
+++ src/memery_db.erl
@@ -1,5 +1,5 @@
 -module(memery_db).
--export([install/1]).
+-export([install/1, store_meme/5]).
 
 -record(meme, {uuid :: uuid:uuid(),
                name :: string(),
@@ -16,3 +16,15 @@ install(Nodes) ->
                          {index, [#meme.name]},
                          {disc_copies, Nodes}]),
     rpc:multicall(Nodes, application, stop, [mnesia]).
+
+store_meme(Name, Description, Tags, Source, Outpath) ->
+    [Type|_] = lists:reverse(string:split(Source, ".", all)),
+    UUID = uuid:get_v4(),
+    Output = Outpath ++ "/" ++ UUID ++ "." ++ Type,
+    {ok, _} = file:copy(Source, Output),
+    Meme = #meme{uuid=UUID,
+                 name=Name,
+                 description=Description,
+                 tags=Tags,
+                 path=Output},
+    mnesia:activity(transaction, fun() -> mnesia:write(Meme) end).
